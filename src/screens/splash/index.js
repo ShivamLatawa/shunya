4e3a1d4fcbb8effbc0b23f0c0a1d3ff61299e26c
Shivam Latawa <shivam.latawa@Shivams-MacBook-Pro.local>
2020-08-30 12:23:14 +0530

    Retreve token form storage on splash

--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,26 +1,25 @@
 import React, {useState, useEffect} from 'react';
-import {View, ActivityIndicator, StyleSheet, Text} from "react-native";
-import {Colors} from "react-native/Libraries/NewAppScreen";
-
+import {View, ActivityIndicator, StyleSheet, Text} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export const SplashScreen = ({navigation}) => {
-
     useEffect(() => {
         setTimeout(() => {
-            navigation.navigate('Auth')
+            AsyncStorage.getItem('user_token').then((token) => {
+                token
+                    ? navigation.navigate('Home')
+                    : navigation.navigate('Auth');
+            });
         }, 3000);
     }, []);
 
     return (
         <View style={styles.container}>
             <Text style={styles.brand}>Shunya</Text>
-            <ActivityIndicator
-                color="#FFFFFF"
-                size="large"
-            />
+            <ActivityIndicator color="#FFFFFF" size="large" />
         </View>
     );
-
 };
 
 const styles = StyleSheet.create({
